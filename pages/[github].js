9934022f662937be0a9b2f8b1c9d5ab1a8788071
Georgey <68821192+GeoBrodas@users.noreply.github.com>
2022-07-14 08:24:02 +0530

    add dev tree profile page with static site generation

---
+++ pages/[github].js
@@ -0,0 +1,42 @@
+import { PrismaClient } from '@prisma/client';
+
+function DevTreeProfilePage({ user }) {
+  return <div>DevTreeProfilePage</div>;
+}
+
+export async function getStaticPaths() {
+  const prisma = new PrismaClient();
+
+  const users = await prisma.user.findMany();
+
+  const paths = users.map((user) => ({
+    params: {
+      github: user.github,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { github } = context.params;
+
+  const prisma = new PrismaClient();
+
+  const user = await prisma.user.findUnique({
+    where: {
+      github: github,
+    },
+  });
+
+  return {
+    props: {
+      user,
+    },
+  };
+}
+
+export default DevTreeProfilePage;
